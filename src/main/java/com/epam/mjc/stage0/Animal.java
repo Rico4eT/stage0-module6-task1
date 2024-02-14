c031c392c16b8b8169cb7c4127e16de02f4a1263
Rico4eT <shiwawa96@mail.ru>
2024-02-14 19:46:51 +0500

    My First OOP task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String paw = " paws";
+        String fur = " and a fur.";
+        if (!hasFur)
+            fur = " and no fur.";
+        if (numberOfPaws == 1)
+            paw = " paw";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + paw + fur;
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        System.out.println(dog.getDescription());
+        System.out.println(bird.getDescription());
+    }
 }
